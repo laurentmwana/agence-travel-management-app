0e5c2016fc71d88f0efe23894b5e501e26a16cba
Labeya <laurentmwn@gmail.com>
2025-11-27 20:14:37 +0100

    backend: add route for json-response and update route for itinerary

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ApiBaseController;
 use App\Http\Controllers\DestinationController;
+use App\Http\Controllers\ItineraryController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -10,6 +12,13 @@ Route::get('/', function () {
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
+
+// JSON REPONSE 
+Route::prefix('json-reponse')->name('json.')->group(function () {
+    Route::get('destinations', [ApiBaseController::class, 'destinations'])->name('destination.index');
+    Route::get('itineraries-types', [ApiBaseController::class, 'itinerariesTypes'])->name('itinerary.type');
+});
+// END JSON REPONSE
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -22,7 +31,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::put('destination/{id}/update', [DestinationController::class, 'update'])->name('destination.update');
     Route::delete('destination/{id}/destroy', [DestinationController::class, 'destroy'])->name('destination.destroy');
     // END DESTINATION ROUTE 
+    // ITINERARY ROUTE
+    Route::resource('itinerary', ItineraryController::class)
+        ->parameter('itinerary', 'id');
+    // END ITINERARY ROUTE
 
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
