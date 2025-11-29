2afb9f6363ee8f622a423411d7322caed6873dbb
Labeya <laurentmwn@gmail.com>
2025-11-29 23:34:22 +0100

    backend: handle error exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -23,5 +25,22 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+            $status = $response->getStatusCode();
+            $isProdudction = !app()->environment(['local', 'testing']) && in_array($status, [500, 503, 404, 403]);
+
+            if ($isProdudction) {
+                return inertia('error', [
+                    'status'  => $status,
+                ])->toResponse($request)->setStatusCode($status);
+            }
+
+            if ($status === 419) {
+                return back()->with([
+                    'warning' => 'The page expired, please try again.',
+                ]);
+            }
+
+            return $response;
+        });
     })->create();
